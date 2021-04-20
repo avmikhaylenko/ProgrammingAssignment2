6a3bcd4dba835b4d6fa8e8d1ee563b4a98277552
avmikhaylenko <82891263+avmikhaylenko@users.noreply.github.com>
2021-04-20 20:02:25 +0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,72 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two functions. One of them creates special "matrix"
+## objects and and caches inverse of matrix. The second one retrieves the
+## inverse of matrix from cache or calculates it
 
-## Write a short comment describing this function
 
+## This function creates a special "matrix" object that can cache
+##its inverse. 
+## 'x' is a matrix for caching. 'makeCacheMatrix' is really containing 
+## the list of functions for setting the value of the matrix, for getting 
+## the value of the matrix, for setting the value of inverse of the matrix
+## and for getting the value of inverse of the matrix
+ 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        ## 's' is a variable for inverse of matrix
+        
+        s <- NULL
+        
+        set <- function(y)
+        {
+                x <<- y
+                s <<- NULL
+        }
+        
+        get <- function() x
+        
+        setsolve <- function(solve) s <<- solve
+        
+        getsolve <- function() s
+        
+        list(set = set, get = get, setsolve = setsolve,
+             getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## 'cacheSolve' is a function for computing of inverse of the matrix.
+## If the inverse of the matrix has already been calculated, it gets
+## the inverse of the matrix from the cache. Also the function checks
+## if the matrix is invertible
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        s <- x$getsolve()
+        
+        if (!is.null(s))
+        {
+                message("getting cached data")
+                return(s)
+        }
+        
+        else
+        {
+                ##Checking the matrix is invertible
+                
+                if (ncol(x$get()) != nrow(x$get()))
+                {
+                        message("The matrix is not invertible")
+                        s <- NA
+                }
+                
+                else
+                {
+                    s <- solve(x$get())
+                    x$setsolve(s)
+                }   
+        }
+        
+        ## Return a matrix that is the inverse of 'x' or NA
+        
+        s
 }
